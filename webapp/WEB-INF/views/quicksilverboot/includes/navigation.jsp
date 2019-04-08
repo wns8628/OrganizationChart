29738f8cd514031e2c28187915497d9d3f1aa354
jooyoung <kjyoung4102@gmail.com>
2019-04-08 16:57:49 +0900

    js 나눔

--- webapp/WEB-INF/views/quicksilverboot/includes/navigation.jsp
+++ webapp/WEB-INF/views/quicksilverboot/includes/navigation.jsp
@@ -3,7 +3,8 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>  
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    
+<script type="text/javascript" src="${pageContext.request.contextPath }/assets/js/quicksilver-ajax-js/quicksilver-nav.js"></script>
+<script type="text/javascript" src="${pageContext.request.contextPath }/assets/js/quicksilver-ajax-js/quicksilver-ajax.js"></script>
 <div class="sidebar navbar-nav">
 	<c:forEach items="${companyList }" var="vo">
 		<div id="company" class="btn-success" data-no='${vo.no }'>${vo.name }</div>
--- webapp/assets/js/quicksilver-ajax-js/quicksilver-ajax.js
+++ webapp/assets/js/quicksilver-ajax-js/quicksilver-ajax.js
@@ -1,13 +1,3 @@
-var render = function(vo){
-   var htmls = "<li id='departments' class='btn-success' class='dept' data-no='"+vo.no+"' g-no='"+vo.gNo+"' p-no='"+vo.parents+"' depth='"+vo.depth+"' style='padding-left:"+vo.depth*10+"px'>"+vo.name+"</li>" +
-   			   "<ul data-no='"+vo.no+"'></ul>";
-
-   if(vo.parents > 0){
-	   $("ul[data-no='"+vo.parents+"']").append(htmls);
-   }else{
-	   $("ul[data-no='"+vo.companyNo+"']").append(htmls);
-   }
-}
 
 var renderTableDepartmentName = function(departmentName){
 	   $(".card-header").empty();
@@ -17,24 +7,6 @@ var renderTableDepartmentName = function(departmentName){
 	   				"</h6>";
 	   				
 	   $(".card-header").append(htmls);
-	}
-
-var getList = function(parents){
-	   $.ajax({
-	      url: contextPath + "/getDept/" + parents,
-	      type:"get",
-	      dataType:"json",
-	      data:"",
-	      success: function(response){
-	    	  console.log(response);
-	         $(response.data).each(function(index, vo){
-	            render(vo)
-	         });
-	      },
-	      error: function(xhr, status, e){
-	         console.error(status+":"+e);
-	      }
-	   });
 	}
 
 var makeTable = function(url) {	
@@ -87,24 +59,8 @@ $(function(){
 	
 	$('#dataTable').dataTable();
 
-   //자회사 목록
-   $(document).on("click", "#company", function(event){
-	   var no = $(this).attr("data-no") * -1;
-	   if($(this).next().children().length > 0){
-		   $(this).next().children().remove();
-	   }else{
-		   getList(no);
-	   }
-   });
-   
    //부서 목록
    $(document).on("click", "#departments", function(event){
-      var no = $(this).attr("data-no");
-      if($(this).next().children().length > 0){
-		   $(this).next().children().remove();
-	  }else{
-		   getList(no);
-	  }
 
    // * 부서 클릭시 테이블에 사원 출력	       	   
 	  console.log("부서 클릭");
