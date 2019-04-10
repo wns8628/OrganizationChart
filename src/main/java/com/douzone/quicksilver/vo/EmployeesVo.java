14aa09e31469aadaf8ece5d28fa93648d2e38223
jooyoung <kjyoung4102@gmail.com>
2019-04-10 20:44:05 +0900

    vo 수정

--- src/main/java/com/douzone/quicksilver/vo/EmployeesVo.java
+++ src/main/java/com/douzone/quicksilver/vo/EmployeesVo.java
@@ -2,31 +2,29 @@ package com.douzone.quicksilver.vo;
 
 public class EmployeesVo {
 
-	private int no;  		   //번호
-	private String profile;	   //프로필	
-	private String name;       //이름
-	private int age;		   //나이
-	private String gender;	   //성별 
-	private String grade;      //직급
-	private String email;      //이메일
-	private String phone;      //전화번호
-	private String address;    //주소
-	private String hire_date;  //고용일
-	private int salary;		   //연봉
+	private Long no;  		   		// 번호
+	private String name;       		// 이름
+	private Long age;		   		// 나이
+	private String gender;	   		// 성별 
+	//private String grade;      	// 직급
+	private Long departmentsNo; 	// 부서번호
+	private String phone;      		// 전화번호
+	private String address;			// 주소
+	private String residentNum; 	// 주민번호
+	private String profile;			// 프로필 사진
+	private String email;			// 이메일
+	private String hireDate;		// 입사일
+	
+	private Long salary;		   //연봉
+	//조인 
 	private String departments;      //부서이름
-	private String company;          //자회사	
+	private String company;          //모회사	
 	private String masterGroup;      //모회사
-	public int getNo() {
+	public Long getNo() {
 		return no;
 	}
-	public void setNo(int no) {
+	public void setNo(Long no) {
 		this.no = no;
-	}
-	public String getProfile() {
-		return profile;
-	}
-	public void setProfile(String profile) {
-		this.profile = profile;
 	}
 	public String getName() {
 		return name;
@@ -34,10 +32,10 @@ public class EmployeesVo {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getAge() {
+	public Long getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+	public void setAge(Long age) {
 		this.age = age;
 	}
 	public String getGender() {
@@ -46,17 +44,11 @@ public class EmployeesVo {
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-	public String getGrade() {
-		return grade;
+	public Long getDepartmentsNo() {
+		return departmentsNo;
 	}
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
+	public void setDepartmentsNo(Long departmentsNo) {
+		this.departmentsNo = departmentsNo;
 	}
 	public String getPhone() {
 		return phone;
@@ -70,17 +62,29 @@ public class EmployeesVo {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getHire_date() {
-		return hire_date;
+	public String getResidentNum() {
+		return residentNum;
 	}
-	public void setHire_date(String hire_date) {
-		this.hire_date = hire_date;
+	public void setResidentNum(String residentNum) {
+		this.residentNum = residentNum;
 	}
-	public int getSalary() {
-		return salary;
+	public String getProfile() {
+		return profile;
 	}
-	public void setSalary(int salary) {
-		this.salary = salary;
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
 	}
 	public String getDepartments() {
 		return departments;
@@ -102,11 +106,19 @@ public class EmployeesVo {
 	}
 	@Override
 	public String toString() {
-		return "EmployeesVo [no=" + no + ", profile=" + profile + ", name=" + name + ", age=" + age + ", gender="
-				+ gender + ", grade=" + grade + ", email=" + email + ", phone=" + phone + ", address=" + address
-				+ ", hire_date=" + hire_date + ", salary=" + salary + ", departments=" + departments + ", company="
-				+ company + ", masterGroup=" + masterGroup + "]";
+		return "EmployeesVo [no=" + no + ", name=" + name + ", age=" + age + ", gender=" + gender + ", departmentsNo="
+				+ departmentsNo + ", phone=" + phone + ", address=" + address + ", residentNum=" + residentNum
+				+ ", profile=" + profile + ", email=" + email + ", hireDate=" + hireDate + ", departments="
+				+ departments + ", company=" + company + ", masterGroup=" + masterGroup + "]";
+	}
+	public Long getSalary() {
+		return salary;
+	}
+	public void setSalary(Long salary) {
+		this.salary = salary;
 	}
 	
 	
+	
+
 }
