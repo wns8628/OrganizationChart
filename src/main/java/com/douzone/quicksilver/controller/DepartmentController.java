44bf1d58ba21d0b81c5d65d1f7e0cc298367f10f
KIMTAEKJU <wpaltm12@naver.com>
2019-04-10 20:29:29 +0900

    2019-04-10 직원 insert문 완료

--- src/main/java/com/douzone/quicksilver/controller/DepartmentController.java
+++ src/main/java/com/douzone/quicksilver/controller/DepartmentController.java
@@ -14,7 +14,7 @@ import com.douzone.quicksilver.vo.EmployeesVo;
 public class DepartmentController {
 	
 	@Autowired
-	DepartmentService departmentService;
+	private DepartmentService departmentService;
 	
 	// 부서 정보를 가져와서 테이블에 출력
 	@ResponseBody
