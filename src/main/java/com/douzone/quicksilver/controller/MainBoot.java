ad45aae931798e865f3bd149e277b02f331e95fd
jooyoung <kjyoung4102@gmail.com>
2019-04-24 19:23:38 +0900

    z

--- src/main/java/com/douzone/quicksilver/controller/MainBoot.java
+++ src/main/java/com/douzone/quicksilver/controller/MainBoot.java
@@ -1,5 +1,7 @@
 package com.douzone.quicksilver.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,8 +19,13 @@ public class MainBoot {
 	NaviService naviService;
 	
 	@RequestMapping("/boot")
-	public String main(Model model) {
-		model.addAttribute("companyList", naviService.getCompList());
+	public String main(Model model, HttpSession session) {
+		String langCode = (String) session.getAttribute("langCode");
+		if(langCode == null) {
+			langCode = "kr";
+		}
+		System.out.println(langCode);
+		model.addAttribute("companyList", naviService.getCompList(langCode));
 		return "quicksilverboot/main";
 	}
 	
