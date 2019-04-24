96067c822a0b8282ec1d31150dbe8d982cd7a434
jooyoung <kjyoung4102@gmail.com>
2019-04-24 10:23:08 +0900

    vo

--- src/main/java/com/douzone/quicksilver/vo/CompanyVo.java
+++ src/main/java/com/douzone/quicksilver/vo/CompanyVo.java
@@ -2,32 +2,285 @@ package com.douzone.quicksilver.vo;
 
 public class CompanyVo {
 
-	private long no;
-	private String name;
-	private long groupNo;
+	private String compSeq;
+	private String groupSeq;
+	private String compCd;
+	private String parentCompSeq;
+	private String loginType;
+	private String compRegistNum;
+	private String compNum;
+	private String standardCode;
+	private String erpUse;
+	private String telNum;
+	private String faxNum;
+	private String homepgAddr;
+	private String CompMailUrl;
+	private String compDomain;
+	private String emailAddr;
+	private String emailDomain;
+	private String zipCode;
+	private String smsUse;
+	private String smsId;
+	private String smsPasswd;
+	private String nativeLangCode;
+	private int orderNum;
+	private String useYn;
+	private String eaType;
+	private String createSeq;
+	private String createDate;
+	private String modifyDate;
+	private String compEmailYn;
+	private String erpUseYn;
+	private String smsUseYn;
+	private String langCode;
+	private String compName;
+	private String ownerName;
+	private String senderName;
+	private String bizCondition;
+	private String item;
+	private String addr;
+	private String detailAddr;
 	
-	public long getNo() {
-		return no;
+	public String getCompSeq() {
+		return compSeq;
 	}
-	public void setNo(long no) {
-		this.no = no;
+	public void setCompSeq(String compSeq) {
+		this.compSeq = compSeq;
 	}
-	public String getName() {
-		return name;
+	public String getGroupSeq() {
+		return groupSeq;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setGroupSeq(String groupSeq) {
+		this.groupSeq = groupSeq;
 	}
-	public long getGroupNo() {
-		return groupNo;
+	public String getCompCd() {
+		return compCd;
 	}
-	public void setGroupNo(long groupNo) {
-		this.groupNo = groupNo;
+	public void setCompCd(String compCd) {
+		this.compCd = compCd;
+	}
+	public String getParentCompSeq() {
+		return parentCompSeq;
+	}
+	public void setParentCompSeq(String parentCompSeq) {
+		this.parentCompSeq = parentCompSeq;
+	}
+	public String getLoginType() {
+		return loginType;
+	}
+	public void setLoginType(String loginType) {
+		this.loginType = loginType;
+	}
+	public String getCompRegistNum() {
+		return compRegistNum;
+	}
+	public void setCompRegistNum(String compRegistNum) {
+		this.compRegistNum = compRegistNum;
+	}
+	public String getCompNum() {
+		return compNum;
+	}
+	public void setCompNum(String compNum) {
+		this.compNum = compNum;
+	}
+	public String getStandardCode() {
+		return standardCode;
+	}
+	public void setStandardCode(String standardCode) {
+		this.standardCode = standardCode;
+	}
+	public String getErpUse() {
+		return erpUse;
+	}
+	public void setErpUse(String erpUse) {
+		this.erpUse = erpUse;
+	}
+	public String getTelNum() {
+		return telNum;
+	}
+	public void setTelNum(String telNum) {
+		this.telNum = telNum;
+	}
+	public String getFaxNum() {
+		return faxNum;
+	}
+	public void setFaxNum(String faxNum) {
+		this.faxNum = faxNum;
+	}
+	public String getHomepgAddr() {
+		return homepgAddr;
+	}
+	public void setHomepgAddr(String homepgAddr) {
+		this.homepgAddr = homepgAddr;
+	}
+	public String getCompMailUrl() {
+		return CompMailUrl;
+	}
+	public void setCompMailUrl(String compMailUrl) {
+		CompMailUrl = compMailUrl;
+	}
+	public String getCompDomain() {
+		return compDomain;
+	}
+	public void setCompDomain(String compDomain) {
+		this.compDomain = compDomain;
+	}
+	public String getEmailAddr() {
+		return emailAddr;
+	}
+	public void setEmailAddr(String emailAddr) {
+		this.emailAddr = emailAddr;
+	}
+	public String getEmailDomain() {
+		return emailDomain;
+	}
+	public void setEmailDomain(String emailDomain) {
+		this.emailDomain = emailDomain;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getSmsUse() {
+		return smsUse;
+	}
+	public void setSmsUse(String smsUse) {
+		this.smsUse = smsUse;
+	}
+	public String getSmsId() {
+		return smsId;
+	}
+	public void setSmsId(String smsId) {
+		this.smsId = smsId;
+	}
+	public String getSmsPasswd() {
+		return smsPasswd;
+	}
+	public void setSmsPasswd(String smsPasswd) {
+		this.smsPasswd = smsPasswd;
+	}
+	public String getNativeLangCode() {
+		return nativeLangCode;
+	}
+	public void setNativeLangCode(String nativeLangCode) {
+		this.nativeLangCode = nativeLangCode;
+	}
+	public int getOrderNum() {
+		return orderNum;
+	}
+	public void setOrderNum(int orderNum) {
+		this.orderNum = orderNum;
+	}
+	public String getUseYn() {
+		return useYn;
+	}
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+	public String getEaType() {
+		return eaType;
+	}
+	public void setEaType(String eaType) {
+		this.eaType = eaType;
+	}
+	public String getCreateSeq() {
+		return createSeq;
+	}
+	public void setCreateSeq(String createSeq) {
+		this.createSeq = createSeq;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public String getCompEmailYn() {
+		return compEmailYn;
+	}
+	public void setCompEmailYn(String compEmailYn) {
+		this.compEmailYn = compEmailYn;
+	}
+	public String getErpUseYn() {
+		return erpUseYn;
+	}
+	public void setErpUseYn(String erpUseYn) {
+		this.erpUseYn = erpUseYn;
+	}
+	public String getSmsUseYn() {
+		return smsUseYn;
+	}
+	public void setSmsUseYn(String smsUseYn) {
+		this.smsUseYn = smsUseYn;
 	}
 	
+	public String getLangCode() {
+		return langCode;
+	}
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+	public String getCompName() {
+		return compName;
+	}
+	public void setCompName(String compName) {
+		this.compName = compName;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getBizCondition() {
+		return bizCondition;
+	}
+	public void setBizCondition(String bizCondition) {
+		this.bizCondition = bizCondition;
+	}
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+		this.item = item;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getDetailAddr() {
+		return detailAddr;
+	}
+	public void setDetailAddr(String detailAddr) {
+		this.detailAddr = detailAddr;
+	}
 	@Override
 	public String toString() {
-		return "CompanyVo [no=" + no + ", name=" + name + ", groupNo=" + groupNo + "]";
+		return "CompanyVo [compSeq=" + compSeq + ", groupSeq=" + groupSeq + ", compCd=" + compCd + ", parentCompSeq="
+				+ parentCompSeq + ", loginType=" + loginType + ", compRegistNum=" + compRegistNum + ", compNum="
+				+ compNum + ", standardCode=" + standardCode + ", erpUse=" + erpUse + ", telNum=" + telNum + ", faxNum="
+				+ faxNum + ", homepgAddr=" + homepgAddr + ", CompMailUrl=" + CompMailUrl + ", compDomain=" + compDomain
+				+ ", emailAddr=" + emailAddr + ", emailDomain=" + emailDomain + ", zipCode=" + zipCode + ", smsUse="
+				+ smsUse + ", smsId=" + smsId + ", smsPasswd=" + smsPasswd + ", nativeLangCode=" + nativeLangCode
+				+ ", orderNum=" + orderNum + ", useYn=" + useYn + ", eaType=" + eaType + ", createSeq=" + createSeq
+				+ ", createDate=" + createDate + ", modifyDate=" + modifyDate + ", compEmailYn=" + compEmailYn
+				+ ", erpUseYn=" + erpUseYn + ", smsUseYn=" + smsUseYn + "]";
 	}
-
+	
 }
