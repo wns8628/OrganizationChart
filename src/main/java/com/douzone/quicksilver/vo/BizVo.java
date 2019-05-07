21ad5c6926e6721de1734b3c5bbbd6543d85e016
jooyoung <kjyoung4102@gmail.com>
2019-05-07 17:30:25 +0900

    f

--- src/main/java/com/douzone/quicksilver/vo/BizVo.java
+++ src/main/java/com/douzone/quicksilver/vo/BizVo.java
@@ -29,177 +29,253 @@ public class BizVo {
 	private String item;			//종목
 	private String addr;			//우편주소
 	private String detailAddr;		//상세주소
+	
+	private int bizEmpCount; //사업장별 직원수구할때씀
+
 	public String getBizSeq() {
 		return bizSeq;
 	}
-	public void setBizSeq(String bizSeq) {
-		this.bizSeq = bizSeq;
-	}
+
 	public String getBizCd() {
 		return bizCd;
 	}
-	public void setBizCd(String bizCd) {
-		this.bizCd = bizCd;
-	}
+
 	public String getGroupSeq() {
 		return groupSeq;
 	}
-	public void setGroupSeq(String groupSeq) {
-		this.groupSeq = groupSeq;
-	}
+
 	public String getCompSeq() {
 		return compSeq;
 	}
-	public void setCompSeq(String compSeq) {
-		this.compSeq = compSeq;
-	}
+
 	public String getCompRegistNum() {
 		return compRegistNum;
 	}
-	public void setCompRegistNum(String compRegistNum) {
-		this.compRegistNum = compRegistNum;
-	}
+
 	public String getCompNum() {
 		return compNum;
 	}
-	public void setCompNum(String compNum) {
-		this.compNum = compNum;
-	}
+
 	public String getTelNum() {
 		return telNum;
 	}
-	public void setTelNum(String telNum) {
-		this.telNum = telNum;
-	}
+
 	public String getFaxNum() {
 		return faxNum;
 	}
-	public void setFaxNum(String faxNum) {
-		this.faxNum = faxNum;
-	}
+
 	public String getHomepgAddr() {
 		return homepgAddr;
 	}
-	public void setHomepgAddr(String homepgAddr) {
-		this.homepgAddr = homepgAddr;
-	}
+
 	public String getZipCode() {
 		return zipCode;
 	}
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
+
 	public String getDisplayYn() {
 		return displayYn;
 	}
-	public void setDisplayYn(String displayYn) {
-		this.displayYn = displayYn;
-	}
+
 	public String getNativeLangCode() {
 		return nativeLangCode;
 	}
-	public void setNativeLangCode(String nativeLangCode) {
-		this.nativeLangCode = nativeLangCode;
-	}
+
 	public String getOrderNum() {
 		return orderNum;
 	}
-	public void setOrderNum(String orderNum) {
-		this.orderNum = orderNum;
-	}
+
 	public String getUseYn() {
 		return useYn;
 	}
-	public void setUseYn(String useYn) {
-		this.useYn = useYn;
-	}
+
 	public String getSealFileId() {
 		return sealFileId;
 	}
-	public void setSealFileId(String sealFileId) {
-		this.sealFileId = sealFileId;
-	}
+
 	public String getCreateSeq() {
 		return createSeq;
 	}
-	public void setCreateSeq(String createSeq) {
-		this.createSeq = createSeq;
-	}
+
 	public String getCreateDate() {
 		return createDate;
 	}
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
+
 	public String getModifySeq() {
 		return modifySeq;
 	}
-	public void setModifySeq(String modifySeq) {
-		this.modifySeq = modifySeq;
-	}
+
 	public String getModifyDate() {
 		return modifyDate;
 	}
-	public void setModifyDate(String modifyDate) {
-		this.modifyDate = modifyDate;
-	}
+
 	public String getLangCode() {
 		return langCode;
 	}
-	public void setLangCode(String langCode) {
-		this.langCode = langCode;
-	}
+
 	public String getBizName() {
 		return bizName;
 	}
-	public void setBizName(String bizName) {
-		this.bizName = bizName;
-	}
+
 	public String getOwnerName() {
 		return ownerName;
 	}
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
+
 	public String getBizCondition() {
 		return bizCondition;
 	}
-	public void setBizCondition(String bizCondition) {
-		this.bizCondition = bizCondition;
-	}
+
 	public String getSenderName() {
 		return senderName;
 	}
-	public void setSenderName(String senderName) {
-		this.senderName = senderName;
-	}
+
 	public String getBizNickname() {
 		return bizNickname;
 	}
-	public void setBizNickname(String bizNickname) {
-		this.bizNickname = bizNickname;
-	}
+
 	public String getItem() {
 		return item;
 	}
-	public void setItem(String item) {
-		this.item = item;
-	}
+
 	public String getAddr() {
 		return addr;
 	}
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
+
 	public String getDetailAddr() {
 		return detailAddr;
 	}
+
+	public int getBizEmpCount() {
+		return bizEmpCount;
+	}
+
+	public void setBizSeq(String bizSeq) {
+		this.bizSeq = bizSeq;
+	}
+
+	public void setBizCd(String bizCd) {
+		this.bizCd = bizCd;
+	}
+
+	public void setGroupSeq(String groupSeq) {
+		this.groupSeq = groupSeq;
+	}
+
+	public void setCompSeq(String compSeq) {
+		this.compSeq = compSeq;
+	}
+
+	public void setCompRegistNum(String compRegistNum) {
+		this.compRegistNum = compRegistNum;
+	}
+
+	public void setCompNum(String compNum) {
+		this.compNum = compNum;
+	}
+
+	public void setTelNum(String telNum) {
+		this.telNum = telNum;
+	}
+
+	public void setFaxNum(String faxNum) {
+		this.faxNum = faxNum;
+	}
+
+	public void setHomepgAddr(String homepgAddr) {
+		this.homepgAddr = homepgAddr;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public void setDisplayYn(String displayYn) {
+		this.displayYn = displayYn;
+	}
+
+	public void setNativeLangCode(String nativeLangCode) {
+		this.nativeLangCode = nativeLangCode;
+	}
+
+	public void setOrderNum(String orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
+	}
+
+	public void setSealFileId(String sealFileId) {
+		this.sealFileId = sealFileId;
+	}
+
+	public void setCreateSeq(String createSeq) {
+		this.createSeq = createSeq;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+	public void setModifySeq(String modifySeq) {
+		this.modifySeq = modifySeq;
+	}
+
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+
+	public void setLangCode(String langCode) {
+		this.langCode = langCode;
+	}
+
+	public void setBizName(String bizName) {
+		this.bizName = bizName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public void setBizCondition(String bizCondition) {
+		this.bizCondition = bizCondition;
+	}
+
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+
+	public void setBizNickname(String bizNickname) {
+		this.bizNickname = bizNickname;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
 	public void setDetailAddr(String detailAddr) {
 		this.detailAddr = detailAddr;
 	}
+
+	public void setBizEmpCount(int bizEmpCount) {
+		this.bizEmpCount = bizEmpCount;
+	}
+
 	@Override
 	public String toString() {
-		return "BizVo [bizSeq=" + bizSeq + ", bizName=" + bizName + "]";
+		return "BizVo [bizSeq=" + bizSeq + ", bizCd=" + bizCd + ", groupSeq=" + groupSeq + ", compSeq=" + compSeq
+				+ ", compRegistNum=" + compRegistNum + ", compNum=" + compNum + ", telNum=" + telNum + ", faxNum="
+				+ faxNum + ", homepgAddr=" + homepgAddr + ", zipCode=" + zipCode + ", displayYn=" + displayYn
+				+ ", nativeLangCode=" + nativeLangCode + ", orderNum=" + orderNum + ", useYn=" + useYn + ", sealFileId="
+				+ sealFileId + ", createSeq=" + createSeq + ", createDate=" + createDate + ", modifySeq=" + modifySeq
+				+ ", modifyDate=" + modifyDate + ", langCode=" + langCode + ", bizName=" + bizName + ", ownerName="
+				+ ownerName + ", bizCondition=" + bizCondition + ", senderName=" + senderName + ", bizNickname="
+				+ bizNickname + ", item=" + item + ", addr=" + addr + ", detailAddr=" + detailAddr + ", bizEmpCount="
+				+ bizEmpCount + "]";
 	}
+	
 	
 }
