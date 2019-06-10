af80eb5c3b00414ff135b7d7bc4c27189b0d19a6
jooyoung <kjyoung4102@gmail.com>
2019-06-10 17:33:26 +0900

    ff

--- webapp/assets/js/admin/admin.js
+++ webapp/assets/js/admin/admin.js
@@ -327,22 +327,6 @@ function menuActive(){
 		}
 	}
 
-	$("div.parent-menu").click(function() {
-		$this = $(this);
-		$icon1 = $this.children().next();
-		$icon2 = $this.children().next().next();
-		if ($this.next().css("display") == "none") {
-			$this.addClass("active");
-			$this.next().show();
-			$icon1.hide();
-			$icon2.show();
-		} else {
-			$this.removeClass("active");
-			$this.next().hide();
-			$icon1.show();
-			$icon2.hide();
-		}
-	});
 }
 
 let ArrowChange = () => {
