44a29476d88abb453c61c9226d62834a8ce5e093
jooyoung <kjyoung4102@gmail.com>
2019-06-10 17:34:15 +0900

    fs

--- webapp/assets/js/admin/admin.js
+++ webapp/assets/js/admin/admin.js
@@ -326,23 +326,7 @@ function menuActive(){
 					"font-weight", "bold");
 		}
 	}
-	
-	$("div.parent-menu").click(function() {
-		$this = $(this);
-		$icon1 = $this.children().next();
-		$icon2 = $this.children().next().next();
-		if ($this.next().css("display") == "none") {
-			$this.addClass("active");
-			$this.next().show();
-			$icon1.hide();
-			$icon2.show();
-		} else {
-			$this.removeClass("active");
-			$this.next().hide();
-			$icon1.show();
-			$icon2.hide();
-		}
-	});
+
 }
 
 
